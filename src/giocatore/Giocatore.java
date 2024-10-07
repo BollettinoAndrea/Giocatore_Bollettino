ad2ef043dc88afc9fcd34bd63b4506a729415860
bollettino.20159 <bollettinoandrea06@gmail.com>
2024-10-07 13:10:52 +0200

    commit1

--- src/giocatore/Giocatore.java
+++ src/giocatore/Giocatore.java
@@ -1,5 +1,41 @@
 package giocatore;
 
 public class Giocatore {
+    private String nome;
+    private boolean capitano;
+    private int gol_fatti;
+
+    public Giocatore (String name, boolean cap, int gol){
+        nome = name;
+        capitano = cap;
+        gol_fatti = gol;
+    }
+
+    public Giocatore (){
+        nome = null;
+        capitano = false;
+        gol_fatti = 0;
+    }
+
+    public String getNome(){
+        return nome;
+    }
+    public void setNome(String name){
+        nome = name;
+    }
+
+    public boolean isCapitano(){
+        return capitano;
+    }
+    public void setCapitano(boolean cap){
+        capitano = cap;
+    }
+
+    public int getGol_fatti(){
+        return gol_fatti;
+    }
+    public void setGol_fatti(int gol){
+        gol_fatti = gol;
+    }
 
 }
--- src/giocatore/TestGiocatore.java
+++ src/giocatore/TestGiocatore.java
@@ -1,7 +1,32 @@
 package giocatore;
 
+import java.util.Scanner;
+
 public class TestGiocatore {
     public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+
+        Giocatore g1 = new Giocatore();
+        System.out.println("GIOCATORE 1:");
+        System.out.println("nome: "); g1.setNome(in.nextLine());
+        System.out.println("è il capitano? (true/false): "); g1.setCapitano(in.nextBoolean());
+        System.out.println("gol fatti: "); g1.setGol_fatti(in.nextInt());
+
+        Giocatore g2 = new Giocatore();
+        System.out.println("GIOCATORE 2:");
+        System.out.println("nome: "); g2.setNome(in.nextLine());
+        System.out.println("è il capitano? (true/false): "); g2.setCapitano(in.nextBoolean());
+        System.out.println("gol fatti: "); g2.setGol_fatti(in.nextInt());
+
+        Giocatore g3 = new Giocatore();
+        System.out.println("GIOCATORE 3:");
+        System.out.println("nome: "); g3.setNome(in.nextLine());
+        System.out.println("è il capitano? (true/false): "); g3.setCapitano(in.nextBoolean());
+        System.out.println("gol fatti: "); g3.setGol_fatti(in.nextInt());
+
+
 
     }
 }
